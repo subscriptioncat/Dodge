7fea0a8ae9e97d14b19338770a2472f208aee5d1
Hwan007 <ghksdlco@gmail.com>
2023-09-08 20:12:45 +0900

    MonsterController 및 DataManager 클래스 작성

--- Assets/Scripts/Controller/MonsterController.cs
+++ Assets/Scripts/Controller/MonsterController.cs
@@ -1,11 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
-public class MonsterController : MonoBehaviour
+public class MonsterController : TopDownCharacterController
 {
-    public class MonsterPattern : ScriptableObject
+    public List<MonsterPattern> _pattern = new List<MonsterPattern>();
+    public void Pattern()
+    {
+        CallMoveEvnet(_pattern[0].Move());
+    }
+
+    public void Fire()
+    {
+        GameObject bulletModel = DataManager.Instance.Bullet;
+        var bulletFired = GameObject.Instantiate(bulletModel, this.transform, true);
+    }
+    
+    public void Aim()
     {
 
+    }
+    
+    public void Look()
+    {
+
+    }
+    
+    [CreateAssetMenu(fileName = "MonsterPattern", menuName = "Monster/Pattern", order = 0)]
+    public class MonsterPattern : ScriptableObject
+    {
+        public List<Vector2> Positions = new List<Vector2>();
+        private int count = 0;
+        public virtual Vector2 Move()
+        {
+            if (count >= Positions.Count) count = 0;
+            return Positions[count++];
+        }
+        public virtual bool Condition()
+        {
+            return false;
+        }
     }
 }
