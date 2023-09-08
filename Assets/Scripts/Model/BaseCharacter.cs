bc8b7fb333f81c331f890543693e05fb9469940d
kwakminkyu <87299508+kwakminkyu@users.noreply.github.com>
2023-09-08 18:06:23 +0900

    add base
    이런식으로 하는게 맞는지 모르겠네요..

---
+++ Assets/Scripts/Model/BaseCharacter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter
+{
+    string Name { get; }
+    public int HP { get; set; }
+    public int Atk { get; set; }
+    public float AtkSpeed { get; set; }
+    public bool IsDie { get; set; }
+    
+    public BaseCharacter(string name, int hp, int atk) {
+        this.Name = name;
+        this.HP = hp;
+        this.Atk = atk;
+    }
+
+    public virtual void TakeDamage(int damage) {
+        HP -= damage;
+        if (HP <= 0) {
+            IsDie = true;
+        }
+    }
+}
