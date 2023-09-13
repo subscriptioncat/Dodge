f1a33f551de6750a16281fa7d1230f19e58b3910
Hwan007 <ghksdlco@gmail.com>
2023-09-14 04:56:35 +0900

    맞는 소리 추가.

--- Assets/Scripts/Model/MonsterData.cs
+++ Assets/Scripts/Model/MonsterData.cs
@@ -1,3 +1,16 @@
 using UnityEngine;
 
-public class MonsterData : BaseCharacter { }
+public class MonsterData : BaseCharacter
+{
+    private CharacterAudio m_Audio;
+
+    private void Awake()
+    {
+        m_Audio = this.gameObject.GetComponent<CharacterAudio>();
+    }
+    public override void TakeDamage(int player, int damage)
+    {
+        base.TakeDamage(player, damage);
+        m_Audio.PlayHit();
+    }
+}
