0584c7f34b15dd3b1a6c1ccd68d9e6bf8747cf36
KimDoHyeon <hj2471755@naver.com>
2023-09-14 09:09:34 +0900

    플레이어 움직임 제한

--- Assets/Scripts/Entities/TopDownMovement.cs
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -8,11 +8,12 @@ public class TopDownMovement : MonoBehaviour
 
     private Vector2 _movementDirection = Vector2.zero;
     private Rigidbody2D _rigidbody;
-
+    private bool _isPlayer;
     private void Awake()
     {
         _controller = GetComponent<TopDownCharacterController>();
         _rigidbody = GetComponent<Rigidbody2D>();
+        WhoIs();
     }
 
     private void Start()
@@ -34,7 +35,35 @@ public class TopDownMovement : MonoBehaviour
     private void ApplyMovment(Vector2 direction)
     {
         direction = direction * 10;
-
+        RestrictingPlayerMovment();
         _rigidbody.velocity = direction;
+    }
+    private void WhoIs()
+    {
+        var gameObject = GetComponent<PlayerData>();
+        if (gameObject != null)
+            _isPlayer = true;
+    }
+    private void RestrictingPlayerMovment()
+    {
+        if( _isPlayer )
+        {
+            if (_rigidbody.position.x > 5f)
+            {
+                _rigidbody.position = new Vector2(5f, _rigidbody.position.y);
+            }
+            else if(_rigidbody.position.x < -5f)
+            {
+                _rigidbody.position = new Vector2(-5f, _rigidbody.position.y);
+            }
+            if(_rigidbody.position.y > 4f)
+            {
+                _rigidbody.position = new Vector2(_rigidbody.position.x, 4f);
+            }
+            else if(_rigidbody.position.y < -3.5f)
+            {
+                _rigidbody.position = new Vector2(_rigidbody.position.x, -3.5f);
+            }
+        }
     }
 }
